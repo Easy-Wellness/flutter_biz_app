5e077060f3291553d9ab24629d9a2e33d494aa7f
Son Xuan Nguyen <29955043+SonXuanNguyen2k@users.noreply.github.com>
2021-09-08 19:40:28 +0700

    💩 move components that don't need state to StatelessWidget

--- lib/widgets/basic_business_info_form_fields.dart
+++ lib/widgets/basic_business_info_form_fields.dart
@@ -10,7 +10,7 @@ final _emailRegex = RegExp(
   caseSensitive: false,
 );
 
-class BasicBusinessInfoFormFields extends StatefulWidget {
+class BasicBusinessInfoFormFields extends StatelessWidget {
   BasicBusinessInfoFormFields({
     Key? key,
     required this.formValidationManager,
@@ -49,131 +49,140 @@ class BasicBusinessInfoFormFields extends StatefulWidget {
   final void Function(String?) onWebsiteSaved;
 
   @override
-  _BasicBusinessInfoFormFieldsState createState() =>
-      _BasicBusinessInfoFormFieldsState();
-}
-
-class _BasicBusinessInfoFormFieldsState
-    extends State<BasicBusinessInfoFormFields> {
-  GeoLocation? businessLocation;
-  double? spacing;
-
-  @override
-  void initState() {
-    super.initState();
-    businessLocation = widget.initialBusinessLocation;
-    spacing = widget.spacing;
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         ...[
           ClearableTextFormField(
-            initialValue: widget.initialName,
-            validator:
-                widget.formValidationManager.wrapValidator('name', (value) {
+            initialValue: initialName,
+            validator: formValidationManager.wrapValidator('name', (value) {
               if (value == null || value.trim().isEmpty)
                 return 'Name is required';
               if (value.trim().length < 4 || value.trim().length > 64)
                 return 'Name must contain between 4 and 64 characters';
             }),
-            onSaved: widget.onNameSaved,
-            focusNode:
-                widget.formValidationManager.getFocusNodeForField('name'),
+            onSaved: onNameSaved,
+            focusNode: formValidationManager.getFocusNodeForField('name'),
             autovalidateMode: AutovalidateMode.onUserInteraction,
             keyboardType: TextInputType.name,
             decoration: InputDecoration(
-              border: widget.border,
+              border: border,
               labelText: 'Business name',
               helperText: '',
             ),
           ),
-          MaterialButton(
-            padding: const EdgeInsets.all(0),
-            onPressed: () async {
-              FocusManager.instance.primaryFocus?.unfocus();
-              final location = await Navigator.pushNamed(
-                  context, PickLocationScreen.routeName);
-              if (location != null && mounted)
-                setState(() => businessLocation = location as GeoLocation);
-            },
-            child: AbsorbPointer(
-              child: TextFormField(
-                validator: (_) {
-                  if (businessLocation == null) return 'Address is required';
-                },
-                onSaved: (_) =>
-                    widget.onBusinessLocationSaved(businessLocation),
-                keyboardType: TextInputType.streetAddress,
-                readOnly: true,
-                decoration: InputDecoration(
-                  border: const OutlineInputBorder(),
-                  floatingLabelBehavior: (businessLocation != null)
-                      ? FloatingLabelBehavior.always
-                      : null,
-                  labelText: 'Business address',
-                  helperText: '',
-                  suffixIcon: Icon(Icons.chevron_right),
-                  hintText: businessLocation?.address,
-                ),
-              ),
-            ),
+          StreetAddressFormField(
+            initialValue: initialBusinessLocation,
+            onSaved: onBusinessLocationSaved,
           ),
           ClearableTextFormField(
-            initialValue: widget.initialPhoneNumber,
-            validator: widget.formValidationManager.wrapValidator('phoneNumb',
-                (value) {
+            initialValue: initialPhoneNumber,
+            validator:
+                formValidationManager.wrapValidator('phoneNumb', (value) {
               if (value == null || value.isEmpty)
                 return 'Phone number is required';
               if (value.length != 18) return 'Phone number is not valid';
             }),
-            onSaved: widget.onPhoneNumbSaved,
-            focusNode:
-                widget.formValidationManager.getFocusNodeForField('phoneNumb'),
+            onSaved: onPhoneNumbSaved,
+            focusNode: formValidationManager.getFocusNodeForField('phoneNumb'),
             autovalidateMode: AutovalidateMode.onUserInteraction,
             keyboardType: TextInputType.number,
             inputFormatters: [PhoneInputFormatter()],
             decoration: InputDecoration(
-              border: widget.border,
+              border: border,
               labelText: 'Business phone',
               helperText: '',
             ),
           ),
           ClearableTextFormField(
-            initialValue: widget.initialEmail,
-            validator:
-                widget.formValidationManager.wrapValidator('email', (value) {
+            initialValue: initialEmail,
+            validator: formValidationManager.wrapValidator('email', (value) {
               if (value == null || value.trim().isEmpty)
                 return 'Email is required';
               if (value.trim().length < 5 || value.trim().length > 50)
                 return 'Email must contain between 5 and 50 characters';
               if (!_emailRegex.hasMatch(value)) return 'Email is not valid';
             }),
-            onSaved: widget.onEmailSaved,
-            focusNode:
-                widget.formValidationManager.getFocusNodeForField('email'),
+            onSaved: onEmailSaved,
+            focusNode: formValidationManager.getFocusNodeForField('email'),
             autovalidateMode: AutovalidateMode.onUserInteraction,
             keyboardType: TextInputType.emailAddress,
             decoration: InputDecoration(
-              border: widget.border,
+              border: border,
               labelText: 'Business email',
               helperText: '',
             ),
           ),
           ClearableTextFormField(
-            initialValue: widget.initialWebsite,
+            initialValue: initialWebsite,
             keyboardType: TextInputType.url,
-            onSaved: widget.onWebsiteSaved,
+            onSaved: onWebsiteSaved,
             decoration: InputDecoration(
-              border: widget.border,
+              border: border,
               labelText: 'Website (optional)',
             ),
           ),
-        ].expand((widget) => [widget, SizedBox(height: spacing!)])
+        ].expand((widget) => [widget, SizedBox(height: spacing)])
       ],
+    );
+  }
+}
+
+class StreetAddressFormField extends StatefulWidget {
+  const StreetAddressFormField({
+    Key? key,
+    this.initialValue,
+    required this.onSaved,
+  }) : super(key: key);
+
+  final GeoLocation? initialValue;
+  final void Function(GeoLocation?) onSaved;
+
+  @override
+  _StreetAddressFormFieldState createState() => _StreetAddressFormFieldState();
+}
+
+class _StreetAddressFormFieldState extends State<StreetAddressFormField> {
+  GeoLocation? businessLocation;
+
+  @override
+  void initState() {
+    super.initState();
+    businessLocation = widget.initialValue;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      padding: const EdgeInsets.all(0),
+      onPressed: () async {
+        FocusManager.instance.primaryFocus?.unfocus();
+        final location =
+            await Navigator.pushNamed(context, PickLocationScreen.routeName);
+        if (location != null && mounted)
+          setState(() => businessLocation = location as GeoLocation);
+      },
+      child: AbsorbPointer(
+        child: TextFormField(
+          validator: (_) {
+            if (businessLocation == null) return 'Address is required';
+          },
+          onSaved: (_) => widget.onSaved(businessLocation),
+          keyboardType: TextInputType.streetAddress,
+          readOnly: true,
+          decoration: InputDecoration(
+            border: const OutlineInputBorder(),
+            floatingLabelBehavior: (businessLocation != null)
+                ? FloatingLabelBehavior.always
+                : null,
+            labelText: 'Business address',
+            helperText: '',
+            suffixIcon: Icon(Icons.chevron_right),
+            hintText: businessLocation?.address,
+          ),
+        ),
+      ),
     );
   }
 }
