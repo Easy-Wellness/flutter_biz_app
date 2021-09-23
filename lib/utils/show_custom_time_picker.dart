66466fde62647220944df4b49cd740b7ffab3caa
Son Xuan Nguyen <29955043+SonXuanNguyen2k@users.noreply.github.com>
2021-09-23 16:40:41 +0700

    🚚 shared util should be in a shared folder

--- lib/utils/show_custom_time_picker.dart
+++ lib/utils/show_custom_time_picker.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 void showCustomTimePicker({
   required BuildContext context,
   required int initialTimeInSecs,
+  bool use24hFormat = false,
 
   /// The number of seconds from midnight 12:00 AM
   required void Function(int) onTimeChanged,
@@ -38,6 +39,7 @@ void showCustomTimePicker({
                 child: CupertinoDatePicker(
                   mode: CupertinoDatePickerMode.time,
                   minuteInterval: 5,
+                  use24hFormat: use24hFormat,
                   initialDateTime:
                       today.add(Duration(seconds: initialTimeInSecs)),
                   onDateTimeChanged: (newDateTime) {
