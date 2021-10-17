64e8c640108dd4670cbc136c95822a25cfe91122
Son Xuan Nguyen <29955043+SonXuanNguyen2k@users.noreply.github.com>
2021-10-17 22:21:23 +0700

    💩 the global initialization logic is now decouble from the App widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -5,15 +6,27 @@ import 'package:provider/provider.dart';
 
 import 'notifiers/business_place_id_notifier.dart';
 import 'routes.dart';
-import 'screens/loading/loading_screen.dart';
 import 'screens/login/login_screen.dart';
 import 'screens/set_place_id_app_state/set_place_id_app_state_screen.dart';
 import 'screens/setting_list/setting_list_screen.dart';
 import 'theme.dart';
 import 'utils/navigate_to_root_screen.dart';
 
+final _useEmulators = false;
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp()
+      // ignore: invalid_return_type_for_catch_error
+      .catchError((_) => print('Cannot initialize Firebase'));
+
+  if (_useEmulators) {
+    await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
+
+    /// See https://firebase.flutter.dev/docs/firestore/usage#emulator-usage
+    FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080);
+  }
+
   final businessPlaceIdNotifier = await BusinessPlaceIdNotifier().init();
   runApp(ChangeNotifierProvider(
     create: (_) => businessPlaceIdNotifier,
@@ -27,8 +40,33 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  final Future<FirebaseApp> initFirebaseSdk = Firebase.initializeApp();
   final navigatorKey = GlobalKey<NavigatorState>();
+
+  @override
+  void initState() {
+    /// This is only called once after the widget is mounted.
+    WidgetsBinding.instance!.addPostFrameCallback(
+      /// Assign listener after the SDK is initialized successfully.
+      (_) => FirebaseAuth.instance.authStateChanges().listen((User? user) {
+        if (user == null)
+          navigatorKey.currentState!
+              .pushReplacementNamed(LoginScreen.routeName);
+        else
+          Provider.of<BusinessPlaceIdNotifier>(context, listen: false)
+                      .businessPlaceId ==
+                  null
+              ? navigatorKey.currentState!
+                  .pushReplacementNamed(SetPlaceIdAppStateScreen.routeName)
+              : navigatorKey.currentState!.pushReplacementNamed(
+                  SettingListScreen.routeName,
+                  arguments: {
+                    'rootScreenIndex': RootScreen.settingListScreen.index
+                  },
+                );
+      }),
+    );
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,34 +74,8 @@ class _AppState extends State<App> {
       debugShowCheckedModeBanner: false,
       navigatorKey: navigatorKey,
       title: 'Easy Wellness Biz',
+      home: LoginScreen(),
       theme: theme(),
-      home: FutureBuilder(
-          future: initFirebaseSdk,
-          builder: (_, snapshot) {
-            if (snapshot.hasError) return Text('Cannot initialize Firebase');
-
-            if (snapshot.connectionState == ConnectionState.done) {
-              // Assign listener after the SDK is initialized successfully
-              FirebaseAuth.instance.authStateChanges().listen((User? user) {
-                if (user == null)
-                  navigatorKey.currentState!
-                      .pushReplacementNamed(LoginScreen.routeName);
-                else
-                  Provider.of<BusinessPlaceIdNotifier>(context, listen: false)
-                              .businessPlaceId ==
-                          null
-                      ? navigatorKey.currentState!.pushReplacementNamed(
-                          SetPlaceIdAppStateScreen.routeName)
-                      : navigatorKey.currentState!.pushReplacementNamed(
-                          SettingListScreen.routeName,
-                          arguments: {
-                            'rootScreenIndex': RootScreen.settingListScreen.index
-                          },
-                        );
-              });
-            }
-            return LoadingScreen();
-          }),
       routes: routes,
     );
   }
